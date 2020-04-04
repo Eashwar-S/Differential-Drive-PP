832e000b1895ffe459de1569bcfca3564a230511
mesneym <mesney.mesney@gmail.com>
2020-04-04 17:06:31 -0400

    Need to make turtlebot follow path

--- catkin_ws/src/turtlebot_astar/src/talker.py
+++ catkin_ws/src/turtlebot_astar/src/talker.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7b0214a00e4cb4e079cb230af9d305b07d3a7404280418c95cf6525cbf56f0c
-size 1763
+oid sha256:24de848f96d20606b319cc6eb48801a5c49c880ed20adfc586420724b93c69ce
+size 1762
